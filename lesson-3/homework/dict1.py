d06f68039301b45c72218791de4779b851b32a37
dilrabo2023 <dilrabousubjonova0@gmail.com>
2025-03-04 22:20:58 +0500

    lesson-3. Collections 4.03.2025

--- lesson-3/homework/dict1.py
+++ lesson-3/homework/dict1.py
@@ -1,6 +1,7 @@
 def get_value(dictionary, key):
     return dictionary.get(key, None)
 
-my_dict = {"name": "Dilrabo", "age": 100}
+
+my_dict = {"name": "Dilrabo ", "age": 100}
 print(get_value(my_dict, "name"))
 print(get_value(my_dict, "address"))
